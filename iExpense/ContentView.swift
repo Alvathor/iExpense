d8325fc5f629adc8aaad1d36466be2a580037f93
Juliano <alvarenga.vix@gmail.com>
2019-12-31 16:38:50 -0300

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Juliano Goncalves Alvarenga on 31/12/19.
+//  Copyright © 2019 Ciclic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
